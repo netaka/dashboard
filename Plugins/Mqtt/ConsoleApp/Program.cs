d30ebadbd9e9c93cf7554aeda6f30782654101cb
netaka <developer@netaka.net>
2021-05-21 23:57:42 +0900

    Connect MQTT broker host

---
+++ Plugins/Mqtt/ConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Mqtt;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mqtt = new Class1(args[0]);
+            mqtt.Connect();
+
+            Console.ReadKey();
+        }
+    }
+}
--- Plugins/Mqtt/Mqtt/Class1.cs
+++ Plugins/Mqtt/Mqtt/Class1.cs
@@ -1,8 +1,35 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using MQTTnet;
+using MQTTnet.Client;
+using MQTTnet.Client.Options;
 
 namespace Mqtt
 {
     public class Class1
     {
+        IMqttClient mqttClient;
+        string host;
+        public Class1(string host)
+        {
+            this.host = host;
+        }
+
+        public async void Connect()
+        {
+            var factory = new MqttFactory();
+            mqttClient = factory.CreateMqttClient();
+
+            var options = new MqttClientOptionsBuilder()
+                .WithTcpServer(this.host)
+                .Build();
+
+            mqttClient.UseConnectedHandler(async e =>
+            {
+                Console.WriteLine("Connected!");
+            });
+            await mqttClient.ConnectAsync(options, CancellationToken.None);
+        }
     }
 }
